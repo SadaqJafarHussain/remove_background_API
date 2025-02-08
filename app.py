3f126a7283c700b2faf27f4308fd63135a690d23
Sadaq Jafar <sadaqjafar@Macs-MacBook-Pro.local>
2025-02-08 19:46:41 +0300

    Add Procfile for deployment

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def remove_background():
         return jsonify({"error": "Invalid image file"}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=3000)
